894e0924d7d005ccdde683226f84e1ee8df687c4
Владислав Ефимов <vladislavich2@outlook.com>
2021-03-31 16:02:23 +0700

    GET for all titles

---
+++ Roulette2/Controllers/TitleController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Roulette.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Roulette.DataAccess.Context;
+using System.Text.Json;
+using Roulette.DataAccess.Context.Models;
+
+namespace Roulette.Controllers
+{
+    #region save
+    //Добавление моделей
+    //    using (var db = new RouletteContext())
+    //            {
+    //                //Title title1 = new Title { Name = "Bad_Title_2", Description = "is bad_2" };
+    //                //db.Add(title1);
+    //                //db.SaveChanges();
+
+    //            }
+
+    ////Редактирование моделей
+    //using (var db = new RouletteContext())
+    //{
+    //    Title titleBad2 = db.Titles
+    //        .Where(c => c.Name == "Bad_Title_2")
+    //        .FirstOrDefault();
+
+    //    Tier tierBad = db.Tiers
+    //        .Where(c => c.Name == "Bad")
+    //        .FirstOrDefault();
+    //    tierBad.Titles.Add(titleBad2);
+
+    //    db.SaveChanges();
+    //}
+    #endregion
+    [ApiController]
+    [Route("[api/controller]")]
+    public class TitleController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            string text = "";
+            //Вывод моделей
+            using (var db = new RouletteContext())
+            {
+                List<Title> allTitles = db.Titles.ToList();
+                foreach (Title i in allTitles)
+                {
+                    string jsonTitle = JsonSerializer.Serialize<Title>(i);
+                    text += jsonTitle;
+                }
+            }
+            return text;
+        }
+    }
+}
